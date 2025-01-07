58fd5c9be8be865f3a385492badbef9d7140d257
indivicc <system@indivi.cc>
2025-01-08 00:26:38 +0100

    Fix duplicate export issue in api-client.js

--- utils/api-client.js
+++ utils/api-client.js
@@ -1,17 +1,3 @@
-// Updated db.js to include POSTGRES_URL_NON_POOLING
-import { Client } from 'pg';
-
-const client = new Client({
-  connectionString: process.env.POSTGRES_URL_NON_POOLING, // Ensure this environment variable is set
-});
-
-client.connect().catch((error) => {
-  console.error('Failed to connect to the database:', error);
-});
-
-export default client;
-
-// Updated api-client.js to handle data fetching
 import axios from 'axios';
 
 const apiClient = axios.create({
@@ -40,4 +26,4 @@ export const getProducts = async () => {
   }
 };
 
-export default apiClient;
+export { apiClient }; // Correctly export apiClient as a named export
