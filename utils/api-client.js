c6de55649470b9030299676a093116336e815485
indivicc <system@indivi.cc>
2025-01-08 00:25:12 +0100

    Added correct api call URL

--- utils/api-client.js
+++ utils/api-client.js
@@ -1,8 +1,21 @@
+// Updated db.js to include POSTGRES_URL_NON_POOLING
+import { Client } from 'pg';
+
+const client = new Client({
+  connectionString: process.env.POSTGRES_URL_NON_POOLING, // Ensure this environment variable is set
+});
+
+client.connect().catch((error) => {
+  console.error('Failed to connect to the database:', error);
+});
+
+export default client;
+
 // Updated api-client.js to handle data fetching
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://api.carboncopyoriginals.com/api/',
 });
 
 // Fetch customers
